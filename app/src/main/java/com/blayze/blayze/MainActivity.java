94e1d4702ac4fcd82dbecf29b05bb8dd59e9278b
mejiaj4atwit <mejiaj4@wit.edu>
2024-07-24 22:56:59 -0400

    Home screen implemented

--- app/src/main/java/com/blayze/blayze/MainActivity.java
+++ app/src/main/java/com/blayze/blayze/MainActivity.java
@@ -1,20 +1,18 @@
 package com.blayze.blayze;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.blayze.blayze.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,30 +27,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .setAnchorView(R.id.fab).show();
-            }
-        });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_insights, R.id.nav_journal, R.id.nav_water, R.id.nav_sleep,R.id.nav_meditation)
+                R.id.nav_home, R.id.nav_insights, R.id.nav_journal, R.id.nav_water, R.id.nav_sleep, R.id.nav_meditation)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getId() == R.id.nav_home) {
+                binding.appBarMain.toolbar.setNavigationIcon(null);
+            } else {
+                binding.appBarMain.toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+                binding.appBarMain.toolbar.setNavigationOnClickListener(v -> {
+                    // Manually handle back navigation
+                    getOnBackPressedDispatcher().onBackPressed();
+                });
+            }
+        });
+
+        // Handling back press for fragments
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (!navController.popBackStack()) {
+                    finish();
+                }
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
--- app/src/main/java/com/blayze/blayze/ui/home/HomeFragment.java
+++ app/src/main/java/com/blayze/blayze/ui/home/HomeFragment.java
@@ -1,32 +1,44 @@
 package com.blayze.blayze.ui.home;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
+import com.blayze.blayze.R;
 import com.blayze.blayze.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
+
     private FragmentHomeBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel;
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), binding.welcomeText::setText);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        NavController navController = Navigation.findNavController(view);
+
+        binding.journalButton.setOnClickListener(v -> navController.navigate(R.id.action_homeFragment_to_journalFragment));
+        binding.waterButton.setOnClickListener(v -> navController.navigate(R.id.action_homeFragment_to_waterFragment));
+        binding.insightsButton.setOnClickListener(v -> navController.navigate(R.id.action_homeFragment_to_insightsFragment));
+        binding.sleepButton.setOnClickListener(v -> navController.navigate(R.id.action_homeFragment_to_sleepFragment));
+        binding.meditationButton.setOnClickListener(v -> navController.navigate(R.id.action_homeFragment_to_meditationFragment));
     }
 
     @Override
